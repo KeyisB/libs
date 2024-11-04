b26b98c029b4707cec7790d9df2863f75273fc13
KeyisB <157538058+KeyisB@users.noreply.github.com>
2024-11-05 01:37:40 +0300

    mmb

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
+name = '${LibName}'
+
 setup(
-    name="KeyisBLogging",
-    version="1.0.6",
+    name=name,
+    version='${version}',
     author="KeyisB",
     author_email="keyisb.pip@gmail.com",
-    description="KeyisBLogging",
+    description=name,
     long_description='',
     long_description_content_type="text/markdown",
-    url="https://github.com/KeyisB/libs/tree/main/KeyisBLogging",
-    packages=["KeyisBLogging"],
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    packages=[name],
+    package_dir={'': f'{name}'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.12',
     license="MMB License v1.0",
-    install_requires=[
-        'colorama'
-    ]
+    install_requires= '${install_requires}',
 )
